fa7f25ae94ab2e2d1ee790205ddac448260ca4fd
Jane Tan <tanx5@qut.edu.au>
2020-09-09 16:30:15 +1000

    Read from metadata in zip file

--- ml/imagemachine.py
+++ ml/imagemachine.py
@@ -4,45 +4,64 @@ from fs.zipfs import ZipFS
 from predict import predict_path, predict_image
 from clump import clump
 from PIL import Image
+from pathlib import Path
 
 def main():
-    hasMetadata = False
-    if not hasMetadata:
-        metadata = []
-        metadataFilename = "metadata_created.json"
-    else:
-        metadataFilename = "metadata_created.json"
-        input_file = os.path.join(os.getcwd(), metadataFilename)
+    createMetadata = True
+    source_file = "downloads.zip"
+    source_meta = "../data/jane.txy.json"
+    metadata = []
+    if not createMetadata:
+        input_file = os.path.join(os.getcwd(), source_meta)
         with open(input_file, 'r', encoding="utf8") as f:
             metadata = json.load(f)
+    metadata_out, vgg16_predictions, vgg19_predictions = createMetadataAndModelFilters(source_file, createMetadata, metadata)
+    clusterData = {}
+    tree_vgg16 = clump(vgg16_predictions, metadata_out)
+    tree_vgg19 = clump(vgg19_predictions, metadata_out)
+    clusterData['tree_vgg16'] = tree_vgg16
+    clusterData['tree_vgg19'] = tree_vgg19        
+    writeJSONToFile("clusters.json", clusterData, 'w')
+
+def createMetadataAndModelFilters(source_file, create_metadata, metadata):
     vgg16_predictions = []
     vgg19_predictions = []
-    # walk through zip file, TODO: metadata provided
-    with ZipFS(os.path.join(os.getcwd(),"downloads.zip")) as archive:
-        for apath in archive.walk.files():
-            if not hasMetadata:
+    # walk through zip file
+    with ZipFS(os.path.join(os.getcwd(),source_file)) as archive:
+       ## metadata provided  
+        if len(metadata) > 0:
+            # follow the sequence in metadatas
+            for node in metadata:
+                apath = node['_mediaPath'][0].replace('\\','/')
+                with archive.open(apath,'rb') as file:
+                    output = predict_image(Image.open(file))
+                    vgg16_predictions.append(output[0])
+                    vgg19_predictions.append(output[1])
+        ## no metadata
+        else:
+            for apath in archive.walk.files():
+                # print(apath, type(apath))
                 ## create metadata
-                _mediaPath = apath[1:].replace('/','\\\\')
-                # add to metadata
-                filemeta = {}
-                node = {}
-                # img = Image.open(os.path.join(dirname,outfile))
-                # node['byte_size'] = os.path.getsize(filepath)
-                filemeta['node'] = node
-                filemeta['_mediaPath'] = [_mediaPath]
-                metadata.append(filemeta)
-                writeJSONToFile(metadataFilename, metadata, 'w')
-    #         with archive.open(apath,'rb') as file:
-    #             ## Clustering
-    #             output = predict_image(Image.open(file))
-    #             vgg16_predictions.append(output[0])
-    #             vgg19_predictions.append(output[1])
-    # clusterData = {}
-    # tree_vgg16 = clump(vgg16_predictions, metadata)
-    # tree_vgg19 = clump(vgg19_predictions, metadata)
-    # clusterData['tree_vgg16'] = tree_vgg16
-    # clusterData['tree_vgg19'] = tree_vgg19        
-    # writeJSONToFile("clusters.json", clusterData, 'w')
+                if create_metadata:
+                    _mediaPath = apath[1:].replace('/','\\')
+                    # add to metadata
+                    filemeta = {}
+                    node = {}
+                    # img = Image.open(os.path.join(dirname,outfile))
+                    # node['byte_size'] = os.path.getsize(filepath)
+                    filemeta['node'] = node
+                    filemeta['_mediaPath'] = [_mediaPath]
+                    metadata.append(filemeta)
+
+                with archive.open(apath,'rb') as file:
+                    ## Model Filters
+                    output = predict_image(Image.open(file))
+                    vgg16_predictions.append(output[0])
+                    vgg19_predictions.append(output[1])
+                
+    if create_metadata:
+        writeJSONToFile('metadata.json', metadata, 'w')
+    return metadata, vgg16_predictions, vgg19_predictions
 
 
 def writeJSONToFile(filename, data, mode):
