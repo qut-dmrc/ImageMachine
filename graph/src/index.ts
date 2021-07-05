b4a49d3b7c2bb1295daa8fc3cde61f79c8f287e7
Jane Tan <xueyingt96@gmail.com>
2021-07-05 17:00:36 +1000

    Visualization nav tab

--- graph/src/index.ts
+++ graph/src/index.ts
@@ -1,4 +1,9 @@
-import {Dendrogram, HierarchyDatum} from "./graphs";
+import {
+    ClusterGraph,
+    Dendrogram,
+    BubbleGraph,
+    HierarchyDatum,
+} from "./graphs";
 import * as d3 from "d3";
 
 /**
@@ -14,10 +19,10 @@ class ClusterGraphContainer {
             _image: "",
         },
         family: false,
-        familyRoot: false
+        familyRoot: false,
     };
 
-    private models: {[name: string]: HierarchyDatum}
+    private models: { [name: string]: HierarchyDatum };
 
     // Hierarchy root
     private root = d3.hierarchy(this.data);
@@ -25,59 +30,136 @@ class ClusterGraphContainer {
     // Graph container
     private container = d3.select("#left");
 
-    // Graph elements
-    private svg = this.container
-        .append("svg");
+    // Container size
+    private containerNode = <HTMLElement>this.container.node();
 
-    private graph = this.svg
-        .append("g");
+    private containerSize: [number, number] = [
+        this.containerNode.offsetWidth,
+        this.containerNode.offsetHeight,
+    ];
+
+    // Graph elements
+    private svg = this.container.append("svg");
+
+    private graph = this.svg.append("g");
 
     // Input elements
-    private flatValueElement = <HTMLInputElement>document.querySelector("#flatValue");
-    private compactElement = <HTMLInputElement>document.querySelector("#compactCheck");
+    private flatValueElement = <HTMLInputElement>(
+        document.querySelector("#flatValue")
+    );
+    // private compactElement = <HTMLInputElement>document.querySelector("#compactCheck");
 
     // Cluster visualiser
-    private clusterElement = <HTMLDivElement>document.querySelector("#clusters");
-    private modelElement = <HTMLSelectElement>document.querySelector("#models-dropdown");
+    private clusterElement = <HTMLDivElement>(
+        document.querySelector("#clusters")
+    );
+    private modelElement = <HTMLSelectElement>(
+        document.querySelector("#models-dropdown")
+    );
+
+    // Viz options container
+    private vizOptions = document.querySelectorAll(
+        ".viz-nav a"
+    ) as NodeListOf<HTMLAnchorElement>;
 
     public async run() {
+        this.setupVizTypeListener();
         await this.getData();
         this.createData();
         this.maxClusterDistance();
 
-        const containerNode = <HTMLElement>this.container.node();
+        this.displayViz("dendogram");
+    }
 
-        const containerSize: [number, number] = [
-            containerNode.offsetWidth,
-            containerNode.offsetHeight
-        ];
-        new Dendrogram(this.root, this.graph, this.flatValueElement, this.modelElement, this.models, containerSize);
+    private displayViz(vizType: String) {
+        let clusterGraph: ClusterGraph;
+        if ((vizType = "dendogram")) {
+            clusterGraph = new Dendrogram(
+                this.root,
+                this.graph,
+                this.flatValueElement,
+                this.modelElement,
+                this.models,
+                this.containerSize
+            );
+        }
+        if ((vizType = "bubble")) {
+            clusterGraph = new BubbleGraph(
+                this.root,
+                this.graph,
+                this.flatValueElement,
+                this.modelElement,
+                this.models,
+                this.containerSize
+            );
+        }
+        clusterGraph.draw(this.root);
+    }
+
+    /**
+     * Set up visualisation type listener
+     */
+    private setupVizTypeListener() {
+        this.vizOptions.forEach((viz) => {
+            viz.addEventListener("click", (e) => {
+                e.preventDefault();
+                this.selectViz(viz);
+            });
+        });
+    }
+
+    private selectViz(vizOption: HTMLAnchorElement) {
+        if (!vizOption.classList.contains("selected")) {
+            let prevViz = [...this.vizOptions].filter((viz) =>
+                viz.classList.contains("selected")
+            )[0];
+            prevViz.classList.toggle("selected");
+            vizOption.classList.toggle("selected");
+            //update visualization
+            this.updateViz(parseInt(vizOption.getAttribute("value")));
+        }
+    }
+
+    private updateViz(vizType: Number) {
+        switch (vizType) {
+            case 0:
+                //display dendogram
+                console.log("dendogram");
+                this.displayViz("dendogram");
+                break;
+            case 1:
+                //display bubble
+                console.log("bubble");
+                this.displayViz("bubble");
+                break;
+            default:
+                //display dendogram
+                console.log("dendogram");
+                this.displayViz("dendogram");
+        }
     }
 
     /**
      * Retrieve JSON data
      */
     private async getData() {
-        const popup = d3.select("#fetchPopup")
-            .style("display", "block");
+        const popup = d3.select("#fetchPopup").style("display", "block");
         const timer = setInterval(() => {
-            popup.append("span")
-                .html(". ")
+            popup.append("span").html(". ");
         }, 1000);
         const response = await fetch("clusters.json");
         this.models = await response.json();
         var keys = Object.keys(this.models);
         //available models
         let option;
-        for (let i = 0; i < keys.length; i++){
-            option = document.createElement('option');
+        for (let i = 0; i < keys.length; i++) {
+            option = document.createElement("option");
             option.text = keys[i];
             option.value = keys[i];
             this.modelElement.add(option);
         }
-        this.data = this.models[keys[0]]
-        popup
-            .style("display", "none");
+        this.data = this.models[keys[0]];
+        popup.style("display", "none");
         clearTimeout(timer);
     }
 
@@ -93,14 +175,18 @@ class ClusterGraphContainer {
      */
     private maxClusterDistance() {
         d3.select("#flatSlider")
-            .attr("min", d3.min(this.root.descendants(), d => d.data.distance) || 0)
-            .attr("max", d3.max(this.root.descendants(), d => d.data.distance) || 0);
+            .attr(
+                "min",
+                d3.min(this.root.descendants(), (d) => d.data.distance) || 0
+            )
+            .attr(
+                "max",
+                d3.max(this.root.descendants(), (d) => d.data.distance) || 0
+            );
     }
 
     private createClusterTable(clusters: Array<HierarchyDatum>) {
         const clusterContainer = d3.select(this.clusterElement);
-
-
     }
 }
 
